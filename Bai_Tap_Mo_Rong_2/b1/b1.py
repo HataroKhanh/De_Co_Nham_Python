f17c722296ce5693699345323e2856ae1119ae6d
BuiKhanh2007 <147997236+HataroKhanh@users.noreply.github.com>
2023-11-04 15:57:52 +0700

    Update b1.py
    Sua lai phan max

--- Bai_Tap_Mo_Rong_2/b1/b1.py
+++ Bai_Tap_Mo_Rong_2/b1/b1.py
@@ -4,17 +4,14 @@ fo = open('b1.out','w')
 n = int(fi.readline())
 di = []
 for i in range(n):
-	temp = fi.readline().split()
+	temp = fi.readline().split()# ["Lan",'18/9/2006','8.8']
 	ten = temp[0]
 	namsinh = temp[1]
 	diem = float(temp[2])
-	hoc_sinh = (ten,namsinh,diem)
+	hoc_sinh = [ten,namsinh,diem]
 	di.append(hoc_sinh)
 print(di)
-maxx= 0
-for i in di:
-	if i[2] >= maxx:
-		maxx = i[2]
+dmax = max(temp,key=lambda x:x[2])
 for i in di:
 	if maxx in i:
 		fo.writelines(f"Ho Ten: {i[0]}\n" )
